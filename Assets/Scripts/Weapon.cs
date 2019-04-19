0f30a8c123a7d9a165e639c857ce97627331d13b
Zion Perez <zionperez@gmail.com>
2019-04-19 18:40:47 +0900

    Add tilemap, ground grid, jumping, movement, and attack logic and animation.

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Animator animator;
+    public Transform firePoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Got fire1 button");
+            animator.SetTrigger("Attack");
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+
+    }
+}
