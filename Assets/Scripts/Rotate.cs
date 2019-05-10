712bd77afce05d757ab57d2e4433564d3fe28d5e
Zion Perez <zionperez@gmail.com>
2019-05-10 18:48:16 +0900

    Implement shredder blades with rotation and colliders.

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float speed;
+    public bool clockwise;    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 axis = clockwise ? Vector3.back : Vector3.forward;
+        transform.Rotate(axis: axis, angle: speed * Time.deltaTime);        
+    }
+}
