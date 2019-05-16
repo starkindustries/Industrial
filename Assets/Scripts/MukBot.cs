c39940ee672e61fb83fbf04ed82f3d892b56f940
Zion Perez <zionperez@gmail.com>
2019-05-16 23:49:08 +0900

    Add splat effect to mukbot on death.

---
+++ Assets/Scripts/MukBot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MukBot : MonoBehaviour, Damageable
+{
+    public GameObject splatEffect;
+
+    void Damageable.Damage()
+    {
+        Debug.Log("Mukbot took damage!!");
+        Instantiate(splatEffect, transform.position, transform.rotation);
+        Destroy(this.gameObject);
+    }
+}
