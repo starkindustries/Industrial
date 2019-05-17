bf12814e15c1233e1e8eb2684b2aeef6aaee93a8
Zion Perez <zionperez@gmail.com>
2019-05-17 13:58:34 +0900

    Add fall collider to detect if player falls off the platform and dies.

---
+++ Assets/Scripts/FallCollider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallCollider : MonoBehaviour
+{    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Damageable objectHit = other.gameObject.GetComponent<Damageable>();
+        if (objectHit != null)
+        {
+            objectHit.Damage();
+        }
+    }
+}
